0a58f19b714a3982fe90a7bc7955645f4830cc47
Edmond Shek <edmondshek11@gmail.com>
2022-06-03 15:24:23 -0400

    added check for hey milo

--- app.py
+++ app.py
@@ -8,22 +8,22 @@ from flask import Flask, request
 app = Flask(__name__)
 
 def send_message(msg):
-  url  = 'https://api.groupme.com/v3/bots/post'
-  data = {
-    "bot_id": os.getenv('GROUPME_BOT_ID'),
-    "text": msg
-  }
-  resp = requests.post(url, data=json.dumps(data))
+	url  = "https://api.groupme.com/v3/bots/post"
+	data = {
+		"bot_id": os.getenv('GROUPME_BOT_ID'),
+		"text": msg
+	}
+	resp = requests.post(url, data=json.dumps(data))
 
 @app.route('/', methods=['POST'])
 def webhook():
   
-  data = request.get_json()
-  
-  # We don't want to reply to ourselves!
-  if data['name'] != 'Milo':
-    msg = data['name']
-    send_message(msg)
+	data = request.get_json()
 
-  return "OK", 200
+	# We don't want to reply to ourselves!
+		if data["name"] != "Milo" and message_start == "hey milo":
+			msg = data["name"]
+			send_message(msg)
+
+	return "OK", 200
 
