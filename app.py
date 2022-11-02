f031dbf91efb56aa9c0d9269b3a0d4a2c53afebb
Edmond Shek <edmondshek11@gmail.com>
2022-11-02 02:03:29 -0400

    added apitest groups for possible sunday and wednesday teams

--- app.py
+++ app.py
@@ -21,17 +21,29 @@ TODO (in no specific order of importance):
 # team_info is a map of maps containing information for each of the volleyball teams.
 # The keys for the outside map are the ids for the GroupMe groups.
 team_info = {
-    os.getenv("GROUP_ID_APITEST"): {  # apitest group, mirrors volleybots info
+    os.getenv("GROUP_ID_APITEST0"): {  # apitest0 group, mirrors sunday info
+        "team_name": os.getenv("SUNDAY_TEAM_NAME"),
+        "division_uid": os.getenv("SUNDAY_DIVISION_UID"),
+        "bot_id": os.getenv("GROUPME_BOT_ID_API_TEST_0"),
+        "team_id": os.getenv("SUNDAY_TEAM_ID")
+    },
+    os.getenv("GROUP_ID_APITEST1"): {  # apitest group, mirrors monday info
         "team_name": os.getenv("MONDAY_TEAM_NAME"),
         "division_uid": os.getenv("MONDAY_DIVISION_UID"),
-        "bot_id": os.getenv("GROUPME_BOT_ID_API_TEST"),
+        "bot_id": os.getenv("GROUPME_BOT_ID_API_TEST_1"),
         "team_id": os.getenv("MONDAY_TEAM_ID"),
     },
-    os.getenv("GROUP_ID_APITEST2"): {  # apitest2 group, mirrors dollar store athletes info
+    os.getenv("GROUP_ID_APITEST2"): {  # apitest2 group, mirrors tuesday info
         "team_name": os.getenv("TUESDAY_TEAM_NAME"),
         "division_uid": os.getenv("TUESDAY_DIVISION_UID"),
         "bot_id": os.getenv("GROUPME_BOT_ID_API_TEST_2"),
         "team_id": os.getenv("TUESDAY_TEAM_ID")
+    },
+    os.getenv("GROUP_ID_APITEST3"): {  # apitest3 group, mirrors wednesday info
+        "team_name": os.getenv("WEDNESDAY_TEAM_ID"),
+        "division_uid": os.getenv("WEDNESDAY_TEAM_ID"),
+        "bot_id": os.getenv("GROUPME_BOT_ID_API_TEST_3"),
+        "team_id": os.getenv("WEDNESDAY_TEAM_ID")
     },
     os.getenv("GROUP_ID_VOLLEYBOTS"): {  # volleybots, monday
         "team_name": os.getenv("MONDAY_TEAM_NAME"),
